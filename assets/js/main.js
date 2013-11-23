ade9e9e610287f9f8bd0e3eedb460299f5be3048
Sam Hill <samuhill@gmail.com>
2013-11-23 01:11:45 -0500

    big changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,5 +33,15 @@ function parseXml(xml) {
 //  _price: the price to purchase the item
 //  _title: title of the post
 function generatePost(_link, _price, _title){
-    $("#list").append('<a class="postLink" href="' + _link +'"><span class="post" style="display:block;"><h3 class="title">' + _title +'</h3><p>Price: $' + _price + '</p><i class="fa fa-caret-square-o-right pull-right"></i></span></a>');
+    
+    //collapse code
+    $("#list").append(' <div class="panel panel-default"><div class="panel-heading"> <h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapse' + _title +'">');
+    
+    //item title
+    $("#list").append('<a data-toggle="collapse" data-parent="#accordion" href="#collapse' + _title +'">' + _title + '</a></h4></div>');
+    
+    //item body
+    $("#list").append('<div id="collapseOne" class="panel-collapse collapse in"><div class="panel-body">This is a test</div></div></div>');
+    
 }
+    
