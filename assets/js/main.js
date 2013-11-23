a50e7c694abe40492ba48756d5a319ad7423cf07
Sam Hill <samuhill@gmail.com>
2013-11-22 23:14:47 -0500

    price parsing test

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ function parseXml(xml) {
         //parse title down so it's not super long
         var title = title.substr(title.indexOf("$") + 4);
         //parse price out of title
-        var price = Number(title.replace(/[^0-9\.]+/g,""));
+        var price = title.substr(title.indexOf("$"), title.indexOf(" "));
         console.log(price);
         $("#list").append('<a href="' + link +'"><span style="display:block;"><h2>' + title +'</h2><p>Price: ' + price + '</p><i class="fa fa-caret-square-o-right pull-right"></i></span></a>');
     });
