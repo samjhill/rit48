65ed188a62642f7c3d48ac86084e4fae48dab8c5
Sam Hill <samuhill@gmail.com>
2013-11-22 22:27:11 -0500

    trying to fix duplicate titles

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ $.ajax({
 function parseXml(xml) {
     console.log(xml);
     $(xml).find("item").each(function() {
-        var title = $(this).find("title").text();
+        var title = $(this).find("dc:title").text();
         var link = $(this).find("link").text();
         $("#list").append('<a href="' + link +'"><span style="display:block;"><h2>' + title +'</h2><i class="fa fa-caret-square-o-right pull-right"></i></span></a>');
     });
