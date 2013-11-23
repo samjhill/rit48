c857aea8fd0108648ad21b041c6e640fe721831e
Sam Hill <samuhill@gmail.com>
2013-11-22 22:38:05 -0500

    add price field

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,10 @@ $.ajax({
 function parseXml(xml) {
     console.log(xml);
     $(xml).find("item").each(function() {
-        var title = $(this).find("dc:title").text();
+        var title = $(this).find("title").text();
         var link = $(this).find("link").text();
-        $("#list").append('<a href="' + link +'"><span style="display:block;"><h2>' + title +'</h2><i class="fa fa-caret-square-o-right pull-right"></i></span></a>');
+        //parse cost out of title, need to update it out of +4 to something dynamic
+        var price = title.substr(title.indexOf("$") + 4);
+        $("#list").append('<a href="' + link +'"><span style="display:block;"><h2>' + title +'</h2><p>Price: ' + price + '</p><i class="fa fa-caret-square-o-right pull-right"></i></span></a>');
     });
 }
