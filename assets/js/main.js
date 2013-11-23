84225649b968364099dbd675e26329f83c08f908
Sam Hill <samuhill@gmail.com>
2013-11-22 19:55:07 -0500

    add proxy.php, fixed links to main.js, main.css

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$.ajax({
+    type: "GET",
+    url: "../php/proxy.php",
+    dataType: "xml",
+    success: parseXml
+ });
+
+function parseXml(xml) {
+    console.log(xml);
+    $(xml).find("item").each(function() {
+        var content = $(this).find("title").text()
+        $("#news_list").append('<li>' + content +'</li>');
+    });
+}
---
+++ assets/php/proxy.php
@@ -0,0 +1,19 @@
+<?php
+// Set your return content type
+header('Content-type: application/xml');
+
+// Website url to open
+$daurl = 'http://www.craigslist.org/about/best/all/index.rss';
+
+// Get that website's content
+$handle = fopen($daurl, "r");
+
+// If there is something, read and return
+if ($handle) {
+    while (!feof($handle)) {
+        $buffer = fgets($handle, 4096);
+        echo $buffer;
+    }
+    fclose($handle);
+}
+?>
