5ec5aa43bab7260d040720c065802a02f365b8cf
Sam Hill <samuhill@gmail.com>
2013-11-22 21:24:11 -0500

    New index

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,6 @@ function parseXml(xml) {
     $(xml).find("item").each(function() {
         var title = $(this).find("title").text();
         var link = $(this).find("link").text();
-        $("#list").append('<a href="' + link +'"><span><h2>' + title +'</h2><i class="fa fa-caret-square-o-right pull-right"></i></span></a>');
+        $("#list").append('<a href="' + link +'"><span style="display:block;"><h2>' + title +'</h2><i class="fa fa-caret-square-o-right pull-right"></i></span></a>');
     });
 }
