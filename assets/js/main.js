bdbe4c8117009e6aa9ed607bbef251eb3223ad83
Sam Hill <samuhill@gmail.com>
2013-11-23 15:32:20 -0500

    make more readable

--- assets/js/main.js
+++ assets/js/main.js
@@ -163,7 +163,8 @@ function findEbayItem(search){
 	var stddev = 0;
 	var sum = 0;
 	for( var i = 0; i < priceList.length; i++){
-	    sum = sum + Math.pow((priceList[i] - avgPrice) , 2);
+	    var difference = priceList[i] - avgPrice;
+	    sum = sum + Math.pow((difference) , 2);
 	    //sum = sum + (priceList[i] - avgPrice);
 	}
 	console.log('sum: '+ sum);
