09e39649dc81e3dc5b38d53bc31e2d42a26fc62f
Sam Hill <samuhill@gmail.com>
2013-11-23 02:45:42 -0500

    initial test of ebay API script

--- assets/php/proxy.php
+++ assets/php/proxy.php
@@ -3,7 +3,7 @@
 header('Content-type: application/xml');
 
 // Website url to open
-$daurl = 'http://rochester.craigslist.org/moa/index.rss';
+$daurl = 'http://rochester.craigslist.org/search/?areaID=126&catAbb=sss&query=iphone&sort=rel&format=rss';
 
 // Get that website's content
 $handle = fopen($daurl, "r");
