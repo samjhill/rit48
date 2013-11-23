82253ca8ae258ed3e81de5e6f6dad78868c8aca3
Sam Hill <samuhill@gmail.com>
2013-11-22 23:34:46 -0500

    let's give it a go

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,14 +11,16 @@ function parseXml(xml) {
         var title = $(this).find("title").text();
         var link = $(this).find("link").text();
         //parse title down so it's not super long
-        var title = title.substr(title.indexOf("$") + 4);
+        title = title.substr(title.indexOf("$") + 4);
         //parse price out of title
         var price = title.substr(title.indexOf("$"), title.indexOf(" "));
-        console.log(parseInt(price));
+        
+        price = parseInt(price.substr(1));
+        
         //this will get rid of TRADE items
-        //if (parseInt(price) >= 0) {
+        if ( price >= 0 ) {
             $("#list").append('<a href="' + link +'"><span style="display:block;"><h2>' + title +'</h2><p>Price: ' + price + '</p><i class="fa fa-caret-square-o-right pull-right"></i></span></a>');
-        //}
+        }
         
     });
 }
