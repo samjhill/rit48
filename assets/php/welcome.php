4b85d3003f161541070de941740c00bf927e9f1c
Sam Hill <samuhill@gmail.com>
2013-11-24 09:12:12 -0500

    fix input formatting

--- assets/php/welcome.php
+++ assets/php/welcome.php
@@ -13,10 +13,7 @@
 	</div>
 	<div class="">
             <label>Distance willing to travel:</label>
-	    <small>
-		<ul class ="slide"><input type="text" class="span2 form-control" value="" placeholder="20" data-slider-min="1" data-slider-max="100" data-slider-step="1" data-slider-value="-14" data-slider-orientation="horizontal" data-slider-selection="after"data-slider-tooltip="show">
-		</ul>
-	    </small>
+	    <input type="text" class="span3 form-control" placeholder="20">
 	    </div>
 	<div class="">
 	    <label>Car MPG:</label>
