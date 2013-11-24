370a9a62168c81cea672bbda31a9aba7d6df262e
Sam Hill <samuhill@gmail.com>
2013-11-24 00:35:46 -0500

    add class checkbox to checkboxes

--- assets/php/welcome.php
+++ assets/php/welcome.php
@@ -29,25 +29,25 @@
         <label>I&apos;m interested in these feeds:</label> 
         <div class="checkbox">
             <label>
-            <input type="checkbox" value="">
+            <input type="checkbox" class="checkbox" value="">
             Antiques
             </label>
         </div>    
             <div class="checkbox">
             <label>
-            <input type="checkbox" value="">
+            <input type="checkbox" class="checkbox" value="">
             Autos
             </label>
         </div>
                 <div class="checkbox">
             <label>
-            <input type="checkbox" value="">
+            <input type="checkbox" class="checkbox" value="">
             Clothing
             </label>
         </div>
                     <div class="checkbox">
             <label>
-            <input type="checkbox" value="">
+            <input type="checkbox" class="checkbox" value="">
             Electronics
             </label>
         </div>
