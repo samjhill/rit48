fd4937090bee843cd005ecc29d009b3d99228e36
Nathaniel Mucha <NMucha10@gmail.com>
2013-11-24 01:22:02 -0500

    here ya go

---
+++ assets/php/header.php
@@ -0,0 +1,52 @@
+<?php
+    echo '
+    <header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/assets/php/index.php">MiddleMan</a>
+      
+      <form role="form-search text-center">
+        
+	<div class="input-group">
+	    <input class="search-query form-control" type="search" id="searchBox" placeholder="Search products"></input>
+	    <span class="btn" type="submit" color="white" onclick="$(&apos;#list&apos;).empty(); search(document.getElementById(&apos;searchBox&apos;).value); $(&apos;#hints&apos;).hide();"><i class="fa fa-search"></i></span>
+	<a id="trendingTitle" href="trending.html">Trending</a>
+        </div>
+        
+  
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <!-- This is where the user&apos;s curr loc will go -->
+        <li><a href="#"><h3>Rochester</h3>
+		<small>(change location) </small></a></li>
+        <li><a href="#"><h3>Search Radius</h3></a></li>
+		<small>
+		<ul class ="slide"><input type="text" id="foo" class="span2" value="" data-slider-min="1" data-slider-max="100" data-slider-step="1" data-slider-value="-14" data-slider-orientation="horizontal" data-slider-selection="after"data-slider-tooltip="show">
+		</ul>
+		</small>
+		
+
+		<li> <a href = "#"><h3>Car</h3>
+		<small>MPG, Fuel </small>
+		</a></li>
+		<li><a href = "#"><h3> Feeds </h3></a>
+		<small>
+		<ul class = "subtext">
+		<!-- add these dynamically -->
+		<a href "#" style="color:white;"><i class="fa fa-times" style="color:red;"></i></a> Electronics <br/>
+		<a class = "highlighted" href = "#"><i class="fa fa-plus" style="color:green;"></i> Add Feed </a>
+		</ul>
+		</small>
+		</li>
+	  </ul>
+    </div><!-- /.navbar-collapse -->
+    
+  </header>';
+?>
